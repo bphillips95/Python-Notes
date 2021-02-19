4a5e96d57c4e3b4e8ee0e9e491eec6b475b027c2
Baruch Phillips <baruchphillips@gmail.com>
2021-02-19 00:43:13 -0500

    added notes on loops

--- notes.py
+++ notes.py
@@ -1,2 +1,35 @@
 msg = "Hello World"
-print(msg)
+# print(msg)
+# In python variables can be declared without a declaration
+# print(type(msg))
+# You can convert data types by writing in the data type 
+num = 28764
+# print(msg + str(num))
+
+def aFunc():
+    msg = "This is a function"
+    print(msg)
+
+# aFunc()
+# unlike JS u can declare a variable inside a function
+def compare():
+    x,y = 1000,1000
+    if (x > y):
+        st = "X is greater than Y"
+    elif(x == y):
+        st = "X is the same as Y"
+    else:
+        st = "X is less than Y"
+    print(st)
+    # dont repeat variable name by else statement
+    st = "X is greater than Y" if (x>y) else "X isnt greater than y"
+    print(st)
+# compare()
+food = ["pizza","pasta","soup","chips","hot dogs"]
+def loopFunc():
+    for f in food:
+        if (f == "chips"): break
+        print(f)
+
+
+loopFunc()
