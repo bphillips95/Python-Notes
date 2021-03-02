e68234c00d23c346229ef3b405dc5fbd8eb4b73d
Baruch Phillips <baruchphillips@gmail.com>
2021-03-01 20:48:05 -0600

    notes

--- crash_course.py
+++ crash_course.py
@@ -5,3 +5,25 @@ first_name = "ada"
 last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
 print(full_name)
+# use the title method to capitalize first letters
+print(f"Hello {full_name.title()}")
+# .strip() to strip whitespace from a string
+einstein = "Albert Einstein"
+quote = "A person who never made a mistake never tried anything new."
+print(f"{einstein} once said '{quote}'")
+
+world_age = 5_781
+print(world_age)
+
+# A variable that doesnt change is declared in capital
+DONT_CHANGE = 783259
+
+# def eight():
+#     print(5+3)
+#     print(10-2)
+#     print(2*4)
+#     print(40/5)
+# eight()
+# import this
+# an array in JS is called a list in Python
+
